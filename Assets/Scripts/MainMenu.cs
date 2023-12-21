708c342d90f6d736c545399c65d2f3ba308ea231
Gareth Burger <49961219+gcburger@users.noreply.github.com>
2023-12-21 00:42:37 +0000

    UI: Menu Manager empty and variables for scene names

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,23 +4,28 @@ using UnityEngine.SceneManagement;
 // Class to implement the main menu
 public class MainMenu : MonoBehaviour
 {
+    // Serializing private variables to make them visible and editable in the inspector (but still private to the class)
+    // rather than making them public and thus accessible and editable by other classes
+    [SerializeField] private string moveSceneName = "CharacterMoveScene";
+    [SerializeField] private string placementSceneName = "CharacterPlacementScene";
+    [SerializeField] private string mainMenuSceneName = "MainMenuScene";
+
     // Function to load the CharacterMove scene
     public void CharacterMoveScene()
     {
-        SceneManager.LoadScene("CharacterMoveScene");
+        SceneManager.LoadScene(moveSceneName);
     }
 
     // Function to load the CharacterPlacement scene
         public void CharacterPlacementScene()
     {
-        SceneManager.LoadScene("CharacterPlacementScene");
+        SceneManager.LoadScene(placementSceneName);
     }
 
     // Function to load the MainMenu scene
     public void MainMenuScene()
     {
-        Debug.Log("Main Menu");
-        SceneManager.LoadScene("MainMenuScene");
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 
     // Function to quit the application
